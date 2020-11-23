7bd881ae213a50c41bac7e922a2a68c38d6245a1
gabrieliusmm <55552995+gabrieliusmm@users.noreply.github.com>
2020-11-23 20:46:11 +0200

    new

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // data
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -21,42 +22,80 @@ xhr.send();
 
 const btn = document.querySelector('.btn');
 const conditions = document.querySelector('.weather');
-btn.addEventListener('click',getWeather)
+const list = document.querySelector	(".pasirinkimas")
+//btn.addEventListener('click',getWeather)
+$(document).on('change', 'input', function(){
+    let options = $('datalist')[0].options;
+    for (let i=0;i<options.length;i++){
+       if (options[i].value == $(this).val()) {
+         	conditions.innerHTML = "";
+         	conditions.style.border = "1px solid black";
+			let xhr1 = new XMLHttpRequest();
+			xhr1.onreadystatechange = function() {
+			if (xhr1.readyState === 4 && xhr1.status === 200) {
+				let weather = JSON.parse(xhr1.responseText);
+				let tempsum = 0;
+				let tempamount = 0;
+				let header1 = document.createElement('h2');
+				let textH1 = document.createTextNode(miestas.value + " " +date + " " + "Prognoze:")
+				header1.appendChild(textH1);
+				conditions.appendChild(header1);
+				for (let i = 0; i < weather.forecastTimestamps.length; i++) {
+					if (date === weather.forecastTimestamps[i].forecastTimeUtc.slice(0,10)) {
+						let para = document.createElement('p');
+						let textP = document.createTextNode(weather.forecastTimestamps[i].forecastTimeUtc.slice(10,30) + " " + ":" + " " + weather.forecastTimestamps[i].airTemperature + " " + '\u00B0C' + " " + translate(weather.forecastTimestamps[i].conditionCode));
+						console.log(weather.forecastTimestamps[i].conditionCode);
+						para.appendChild(textP);
+						conditions.appendChild(para);
+						icon(weather.forecastTimestamps[i].conditionCode);
+						let br = document.createElement('br');
+						conditions.appendChild(br);
+						tempsum = tempsum + weather.forecastTimestamps[i].airTemperature;
+						tempamount = tempamount + 1;
+					}
 
-
-function getWeather() {
-	conditions.innerHTML = "";
-	let xhr1 = new XMLHttpRequest();
-	xhr1.onreadystatechange = function() {
-		if (xhr1.readyState === 4 && xhr1.status === 200) {
-			let weather = JSON.parse(xhr1.responseText);
-			let tempsum = 0;
-			let tempamount = 0;
-			let header1 = document.createElement('h2');
-			let textH1 = document.createTextNode(miestas.value + " " +date + " " + "Prognoze:")
-			header1.appendChild(textH1);
-			conditions.appendChild(header1);
-			for (let i = 0; i < weather.forecastTimestamps.length; i++) {
-				if (date === weather.forecastTimestamps[i].forecastTimeUtc.slice(0,10)) {
-					let para = document.createElement('p');
-					let textP = document.createTextNode(weather.forecastTimestamps[i].forecastTimeUtc.slice(10,30) + " " + ":" + " " + weather.forecastTimestamps[i].airTemperature + " " + '\u00B0C' + " " + translate(weather.forecastTimestamps[i].conditionCode));
-					para.appendChild(textP);
-					conditions.appendChild(para);
-					tempsum = tempsum + weather.forecastTimestamps[i].airTemperature;
-					tempamount = tempamount + 1;
 				}
+
+				/*
+				let orai = document.querySelectorAll('p');
+				console.log(orai)
+				*/
+				let para = document.createElement('p');
+				let textP = document.createTextNode("Vidutine Temperatura:" + " " +(tempsum/tempamount).toFixed(1)+" "+"\u00B0C");
+				para.appendChild(textP);
+				conditions.appendChild(para);
 			}
-			let para = document.createElement('p');
-			let textP = document.createTextNode("Vidutine Temperatura:" + " " +(tempsum/tempamount).toFixed(1)+" "+"\u00B0C");
-			para.appendChild(textP);
-			conditions.appendChild(para);
 		}
-}
 
-xhr1.open("GET", "https://api.meteo.lt/v1/places/"+kurismiestas()+"/forecasts/long-term");
-xhr1.send();
-}
 
+		xhr1.open("GET", "https://api.meteo.lt/v1/places/"+kurismiestas()+"/forecasts/long-term");
+		xhr1.send();
+	}
+}
+});
+
+function icon(nameoftheicon) {
+	if (nameoftheicon === "clear" || nameoftheicon === "isolated-clouds" || nameoftheicon === "overcast") {
+		let fa = document.createElement('i');
+		fa.setAttribute("class", "fas fa-sun");
+		conditions.appendChild(fa);
+	}
+		else if (nameoftheicon === "rain" || nameoftheicon === "scattered-clouds" || nameoftheicon === "light-rain" || nameoftheicon === "heavy-rain" || nameoftheicon === "sleet") {
+			let fa = document.createElement('i');
+			fa.setAttribute("class", "fas fa-cloud-rain");
+			conditions.appendChild(fa);
+		}
+			else if (nameoftheicon === "light-snow" || nameoftheicon === "moderate-snow" || nameoftheicon === "heavy-snow") {
+				let fa = document.createElement('i');
+				fa.setAttribute("class", "far fa-snowflake");
+				conditions.appendChild(fa);
+			}
+			else if (nameoftheicon === "fog") {
+				let fa = document.createElement('i');
+				fa.setAttribute("class", "fas fa-smog");
+				conditions.appendChild(fa);
+			}
+}
 function translate(name) {
 	if (name === "clear") {name = "giedra"}
 	else if (name === "isolated-clouds") {name = "mazai debesuota"}
@@ -87,3 +126,5 @@ function kurismiestas() {
 	return which;
 
 }
+
+
